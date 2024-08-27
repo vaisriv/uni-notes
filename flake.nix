ee24ee81c8ae178c52e37da21c79573939169581
vsrivastava <vai.sriv@icloud.com>
2024-08-27 15:42:38 -0400

    initial commit

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  nixConfig = {
+    extra-substituters = "https://cache.garnix.io";
+    extra-trusted-public-keys = "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=";
+  };
+
+  inputs = {
+    emanote.url = "github:srid/emanote";
+    emanote.inputs.emanote-template.follows = "";
+    nixpkgs.follows = "emanote/nixpkgs";
+    flake-parts.follows = "emanote/flake-parts";
+  };
+
+  outputs = inputs@{ self, flake-parts, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [ inputs.emanote.flakeModule ];
+      perSystem = { self', pkgs, system, ... }: {
+        emanote = {
+          # By default, the 'emanote' flake input is used.
+          # package = inputs.emanote.packages.${system}.default;
+          sites."default" = {
+            layers = [{ path = ./.; pathString = "."; }];
+            # port = 8080;
+            baseUrl = "code.umd.edu/vsriv81/uni-24f";
+            # prettyUrls = true;
+          };
+        };
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nixpkgs-fmt
+          ];
+        };
+        formatter = pkgs.nixpkgs-fmt;
+      };
+    };
+}
