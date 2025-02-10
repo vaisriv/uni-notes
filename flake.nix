79407999629d0a273bb8bf1665e6c4c70d5d19d8
vai <vai.sriv@icloud.com>
2025-02-10 10:54:04 -0500

    hopefully changes dont ruin build process testing lol

--- flake.nix
+++ flake.nix
@@ -1,20 +1,33 @@
 {
-	nixConfig = {
-		extra-substituters = "https://cache.garnix.io";
-		extra-trusted-public-keys = "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=";
-	};
-
 	inputs = {
-		emanote.url = "github:srid/emanote";
-		emanote.inputs.emanote-template.follows = "";
-		nixpkgs.follows = "emanote/nixpkgs";
-		flake-parts.follows = "emanote/flake-parts";
+		# Emanote
+		emanote = {
+			url = "github:srid/emanote";
+			inputs.nixpkgs.follows = "nixpkgs";
+			inputs.flake-parts.follows = "flake-parts";
+
+			inputs.emanote-template.follows = "";
+		};
+
+		# Lix
+		lix-module = {
+			url = "https://git.lix.systems/lix-project/nixos-module/archive/2.92.0.tar.gz";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+
+		# Alejandra
+		alejandra = {
+			url = "github:kamadorueda/alejandra";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
 	};
 
 	outputs = inputs @ {
 		self,
-		flake-parts,
 		nixpkgs,
+		flake-parts,
+		lix-module,
+		alejandra,
 		...
 	}:
 		flake-parts.lib.mkFlake {inherit inputs;} {
@@ -40,10 +53,12 @@
 				devShells.default =
 					pkgs.mkShell {
 						buildInputs = [
-							pkgs.nixpkgs-fmt
+							# inputs.alejandra.defaultPackage.${system};
+
+							pkgs.zk
 						];
 					};
-				formatter = pkgs.nixpkgs-fmt;
+				# formatter = inputs.alejandra.defaultPackage.${system};
 			};
 		};
 }
