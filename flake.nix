a00f578a3949be2a42eed24cc80594c843b514d6
vai <vai.sriv@icloud.com>
2025-06-03 17:34:49 -0700

    change formatting standard

--- flake.nix
+++ flake.nix
@@ -1,62 +1,47 @@
 {
-	inputs = {
-		nixpkgs.follows = "emanote/nixpkgs";
-		flake-parts.follows = "emanote/flake-parts";
+  inputs = {
+    nixpkgs.follows = "emanote/nixpkgs";
+    flake-parts.follows = "emanote/flake-parts";
 
-		# Emanote
-		emanote = {
-			url = "github:srid/emanote";
-			inputs.emanote-template.follows = "";
-		};
+    # Emanote
+    emanote = {
+      url = "github:srid/emanote";
+      inputs.emanote-template.follows = "";
+    };
+  };
 
-		# Lix
-		lix-module = {
-			url = "https://git.lix.systems/lix-project/nixos-module/archive/2.92.0.tar.gz";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-
-		# Alejandra
-		alejandra = {
-			url = "github:kamadorueda/alejandra";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-	};
-
-	outputs = inputs @ {
-		self,
-		flake-parts,
-		nixpkgs,
-		lix-module,
-		alejandra,
-		...
-	}:
-		flake-parts.lib.mkFlake {inherit inputs;} {
-			systems = nixpkgs.lib.systems.flakeExposed;
-			imports = [inputs.emanote.flakeModule];
-			perSystem = {
-				self',
-				pkgs,
-				system,
-				...
-			}: {
-				emanote = {
-					sites."default" = {
-						layers = [
-							{
-								path = ./.;
-								pathString = ".";
-							}
-						];
-						prettyUrls = true;
-					};
-				};
-				devShells.default =
-					pkgs.mkShell {
-						packages = with pkgs; [
-							inputs.alejandra.defaultPackage.${system}
-							zk
-						];
-					};
-			};
-		};
+  outputs = inputs @ {
+    self,
+    flake-parts,
+    nixpkgs,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [inputs.emanote.flakeModule];
+      perSystem = {
+        self',
+        pkgs,
+        system,
+        ...
+      }: {
+        emanote = {
+          sites."default" = {
+            layers = [
+              {
+                path = ./.;
+                pathString = ".";
+              }
+            ];
+            prettyUrls = true;
+          };
+        };
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            zk
+          ];
+        };
+        formatter = pkgs.alejandra;
+      };
+    };
 }
